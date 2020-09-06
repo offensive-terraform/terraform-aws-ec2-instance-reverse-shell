fee0a2b205d813b2309ef55f673398d0516d653c
iganbold <itgel.ganbold1@gmail.com>
2020-09-06 10:28:07 -0700

    Add iam_instance_profile

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "this" {
   ami                         = "ami-06b263d6ceff0b3dd"
   instance_type               = "t2.micro"
   subnet_id                   = var.subnet_id
+  iam_instance_profile        = var.iam_instance_profile
   user_data                   = data.template_file.user_data.rendered
   associate_public_ip_address = true
 
--- variables.tf
+++ variables.tf
@@ -3,6 +3,12 @@ variable "subnet_id" {
   description = "The VPC Subnet ID to launch in."
 }
 
+variable "iam_instance_profile" {
+  type        = string
+  description = "The IAM Instance Profile to launch the instance with."
+  default     = null
+}
+
 variable "attacker_ip" {
   type        = string
   description = "The attacker IP to create a reverse shell from AWS EC2 instance."
