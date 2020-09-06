79cd5728841fb8b403ab4929c8aa381fa64e18a6
iganbold <itgel.ganbold1@gmail.com>
2020-09-06 10:53:45 -0700

    Update descriptions of variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "subnet_id" {
   type        = string
-  description = "The VPC Subnet ID to launch in."
+  description = "(Required) The VPC Subnet ID to launch in."
 }
 
 variable "iam_instance_profile" {
   type        = string
-  description = "The IAM Instance Profile to launch the instance with."
+  description = "(Optional) The IAM Instance Profile to launch the instance with."
   default     = null
 }
 
 variable "attacker_ip" {
   type        = string
-  description = "The attacker IP to create a reverse shell from AWS EC2 instance."
+  description = "(Required) The attacker IP to create a reverse shell from AWS EC2 instance."
 }
